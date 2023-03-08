41542ff7f3110c2a54f12b6fa7f86e291e7632c8
Michelle <mzou1223@gmail.com>
2023-03-08 16:14:30 -0500

    made a gameboy thing

--- src/Michelle.js
+++ src/Michelle.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+import anime from "animejs/lib/anime.es.js";
+import { useState } from "react";
 
 export default function Michelle() {
-  return <div>Michelle was here</div>;
+  const animation = useRef(null);
+  const [isBig, setIsBig] = useState(true);
+
+  if (!isBig) {
+    animation.current = anime({
+      targets: '.gameboy',
+      scale: 0.5,
+    });
+  } else {
+    animation.current = anime({
+      targets: '.gameboy',
+      scale: 1,
+    });
+  }
+
+  const handleClick = () => {
+    !isBig ? setIsBig(true) : setIsBig(false);
+    console.log("isBig", isBig)
+  }
+
+  return (
+    <div className="gameboy-container">
+      <img className="gameboy" src="https://cdn-icons-png.flaticon.com/512/1135/1135231.png"></img>
+      <button onClick={handleClick}> Click here to make small!</button>
+    </div>
+
+  )
 }
